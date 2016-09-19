09e8321efff71f81cd72f94cf88794879fd62f3f
b1621ry <b1621ry@aiit.ac.jp>
2016-09-19 06:43:23 +0000

    change file

--- app.rb
+++ app.rb
@@ -1,12 +1,5 @@
 require 'sinatra'
 get '/helloWorld/:name' do
-	case params['name']
-	when 'php' then
-		"print hello world!"
-	when 'ruby' then 
-		"p hello world!"
-	when 'javascript' then
-		"console.log(hello.world!)"
-	end
+	p params['name']
 end
 
